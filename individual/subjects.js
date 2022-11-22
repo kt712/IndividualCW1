0e22f1eabda7a7966a96870d4cba26aeb94173b0
Ismail Kabir <ismailkbalarabe@gmail.com>
2022-11-22 08:50:58 +0400

    js file

---
+++ individual/subjects.js
@@ -0,0 +1,93 @@
+let mySub =[
+
+    {
+        "id": 1001,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1002,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1003,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1004,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1005,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1006,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1007,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1008,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1009,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1010,
+        "title": "Math",
+        "location":"London",
+        "price": 100,
+        "image": "images/cat.jpg",
+        "availableInventory": 5
+       
+      }, 
+]

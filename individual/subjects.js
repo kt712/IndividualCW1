5b87150bae7bdef77626f36bba1322b605bf6631
Ismail Kabir <ismailkbalarabe@gmail.com>
2022-11-22 09:16:35 +0400

    adding images

--- individual/subjects.js
+++ individual/subjects.js
@@ -4,90 +4,90 @@ let mySub =[
         "id": 1001,
         "title": "Math",
         "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "price": 150,
+        "image": "images/math.jpg",
+        "availableInventory": 15
        
       }, 
       {
         "id": 1002,
-        "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "title": "English",
+        "location":"Chelsea",
+        "price": 150,
+        "image": "images/eng.jpg",
+        "availableInventory": 15
        
       }, 
       {
         "id": 1003,
-        "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
+        "title": "Physics",
+        "location":"Oxford",
+        "price": 80,
+        "image": "images/phy.jpg",
         "availableInventory": 5
        
       }, 
       {
         "id": 1004,
-        "title": "Math",
-        "location":"London",
+        "title": "Music",
+        "location":"New York",
         "price": 100,
-        "image": "images/cat.jpg",
+        "image": "images/music.jpg",
         "availableInventory": 5
        
       }, 
       {
         "id": 1005,
-        "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "title": "Web App",
+        "location":"Dubai",
+        "price": 150,
+        "image": "images/web.jpg",
+        "availableInventory": 15
        
       }, 
       {
         "id": 1006,
-        "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "title": "UI/UX",
+        "location":"Sharjah",
+        "price": 90,
+        "image": "images/ui.jpg",
+        "availableInventory": 10
        
       }, 
       {
         "id": 1007,
         "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "location":"Dubai",
+        "price": 90,
+        "image": "images/math.jpg",
+        "availableInventory": 6
        
       }, 
       {
         "id": 1008,
-        "title": "Math",
-        "location":"London",
-        "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "title": "English",
+        "location":"Doha",
+        "price": 85,
+        "image": "images/eng.jpg",
+        "availableInventory": 15
        
       }, 
       {
         "id": 1009,
-        "title": "Math",
+        "title": "Web App",
         "location":"London",
         "price": 100,
-        "image": "images/cat.jpg",
+        "image": "images/web.jpg",
         "availableInventory": 5
        
       }, 
       {
         "id": 1010,
-        "title": "Math",
-        "location":"London",
+        "title": "Economics",
+        "location":"New York",
         "price": 100,
-        "image": "images/cat.jpg",
-        "availableInventory": 5
+        "image": "images/econs.jpg",
+        "availableInventory": 10
        
       }, 
 ]
